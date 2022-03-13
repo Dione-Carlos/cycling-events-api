13bc8c0e719d49a706915efc35f05904247cc5ff
Dione Carlos <dione.csf@gmail.com>
2022-03-13 16:43:59 -0300

    chore: add express

---
+++ src/infra/criptography/db/mongodb/helpers/mongo-helper.ts
@@ -0,0 +1,23 @@
+import { Collection, MongoClient } from 'mongodb'
+
+export const MongoHelper = {
+  client: MongoClient,
+
+  async connect(url: string): Promise<void> {
+    this.client = await MongoClient.connect(url)
+  },
+
+  async disconnect(): Promise<void> {
+    await this.client.close()
+  },
+
+  getCollection(name: string): Collection {
+    return this.client.db().collection(name)
+  },
+
+  map: (collection: any): any => {
+    const { _id, ...collectionWithoutId } = collection
+
+    return Object.assign({}, { id: _id }, collectionWithoutId)
+  }
+}
