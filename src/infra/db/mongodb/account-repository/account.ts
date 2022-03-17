81c9c983b69c855efe61daa4fb757341ced8546e
Dione Carlos <dione.csf@gmail.com>
2022-03-17 00:20:50 -0300

    feat: ensure ExpressRouteAdapter returns the error message instead of the error name

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -6,10 +6,8 @@ import { MongoHelper } from '../helpers/mongo-helper'
 export class AccountMongoRepository implements AddAccountRepository {
   async add(accountData: AddAccountModel): Promise<AccountModel> {
     const accountCollection = await MongoHelper.getCollection('accounts')
-
-    const { insertedId } = await accountCollection.insertOne(accountData)
-
-    const account = await accountCollection.findOne(insertedId)
+    const accountId = await accountCollection.insertOne(accountData)
+    const account = await accountCollection.findOne(accountId.insertedId)
 
     return MongoHelper.map(account)
   }
--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -1,7 +1,8 @@
 import { Controller } from '../../presentation/protocols'
-// import { LogControllerDecorator } from '../decorators/log'
+import { LogControllerDecorator } from '../decorators/log'
 import { BcryptAdapter } from '../../infra/cryptography/bcrypt-adapter'
 import { DbAddAccount } from '../../data/userCases/add-account/db-add-account'
+import { LogMongoRepository } from '../../infra/db/mongodb/log-repository/log'
 import { SignUpController } from '../../presentation/controllers/signup/signup'
 import { EmailValidatorAdapter } from '../../presentation/utils/email-validator-adapter'
 import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
@@ -14,5 +15,5 @@ export const makeSignUpController = (): Controller => {
   const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
   const signUpController = new SignUpController(emailValidatorAdapter, dbAddAccount)
 
-  return signUpController // new LogControllerDecorator(signUpController)
+  return new LogControllerDecorator(signUpController, new LogMongoRepository())
 }
