1cac46169bf603b83dda403aab4a182fb841d9e6
Dione Carlos <dione.csf@gmail.com>
2022-03-17 00:53:01 -0300

    refactor: ajust in code

--- src/presentation/utils/email-validator-adapter.spec.ts
+++ src/presentation/utils/email-validator-adapter.spec.ts
@@ -14,7 +14,9 @@ const makeSut = (): EmailValidatorAdapter => {
 describe('EmailValidator Adapter', () => {
   test('Should return false if validator returns false', () => {
     const sut = makeSut()
+
     jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
+
     const isValid = sut.isValid('invalid_email@mail.com')
 
     expect(isValid).toBe(false)
